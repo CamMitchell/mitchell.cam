c7bffcf2a718364e453781a2fc00b6785a750541
Cameron Mitchell <cammitchell@gmail.com>
2024-01-24 21:09:58 -0500

    initial commit

---
+++ js/commands.js
@@ -0,0 +1,89 @@
+var password = "joshua";
+var linkedin = "https://www.linkedin.com/in/CameronScottMitchell";
+var github = "https://github.com/CamMitchell";
+var twitter = "https://twitter.com/EvilCameron";
+var email = 'mailto:me@CameronScottMitchell.com';
+
+about = [
+  "<br>",
+  "Hi, I'm Cameron",
+  "I'm a software developer, and product architect, building eCommerce solutions for clients in a variety of industries.",
+  "I have worked with companies large and small, that sell everything from from antique tractor parts to highend fashion",
+  "I enjoy the challenge and the unique opportunities that a wide variety of clients allows.", 
+  "For the last 10 years I have specailized in the beverage industry, building full integrated systems.",
+  "I have built integrations to modern systems like Microsoft D365, and Oracle as well as 40 year old AS400 based ERPs",
+  "<br>"
+];
+
+social = [
+  "<br>",
+  'linkedin       <a href="' + linkedin + '" target="_blank">linkedin/CameronScottMitchell' + "</a>",
+  'github         <a href="' + github + '" target="_blank">github/CamMitchell' + "</a>",
+  'twitter        <a href="' + twitter + '" target="_blank">@EvilCameron' + "</a>",
+  "<br>"
+];
+
+games = [
+  "<br>",
+  'Game List',
+  "<br>",  
+  '<span class="command">Falken\'s Maze</span>',
+  '<span class="command">Black Jack</span>',
+  '<span class="command">Gin Rummy</span>',
+  '<span class="command">Hearts</span>',
+  '<span class="command">Bridge</span>',
+  '<span class="command">Checkers</span>',
+  '<span class="command">Chess</span>',
+  '<span class="command">Poker</span>',
+  '<span class="command">Fighter Combat</span>',
+  '<span class="command">Guerrilla Engagement</span>',
+  '<span class="command">Desert Warfare</span>',
+  '<span class="command">Air-To-Ground Actions</span>',
+  '<span class="command">Theaterwide Tactical Warfare</span>',
+  '<span class="command">Theaterwide Biotoxic And Chemical Warfare</span>',
+  "<br>",
+  '<span class="command">Global Thermonuclear War</span>',
+  "<br>"
+];
+
+projects = [
+  "<br>",
+  'Project List',
+  'Here are a few thing I have built just for fun',
+  "<br>",
+  '<a href="https://ctrl-v.ca/" target="_blank">Ctrl-V.ca' + "</a>",
+  'Crtl-v.ca is a simple and free online scratch pad that allows you to store and share information.',
+  "<br>",
+  '<a href="https://minesweeper.plus/" target="_blank">MineSweeper.plus' + "</a>",
+  'An online veriosn of the clascic MineSweeper with and infinite grid',
+  "<br>",
+  '<a href="https://codefaster.io/" target="_blank">CodeFaster.io' + "</a>",
+  'A tool to help Developers write code faster',
+  "<br>"
+];
+
+help = [
+  "<br>",
+  '<span class="command">about</span>          A liitle bit about me',
+  '<span class="command">social</span>         Display social networks',
+  '<span class="command">games</span>          Whould you link to play a game',
+  '<span class="command">projects</span>       View coding projects',
+  '<span class="command">history</span>        View command history',
+  '<span class="command">help</span>           You obviously already know what this does',
+  '<span class="command">clear</span>          Clear terminal',
+  '<span class="command">banner</span>         Display the header',
+  "<br>",
+];
+
+banner = [
+  '<span class="index">http://www.Mitchell.Cam/</span>',
+  "  _____                                       __  __ _ _       _          _ _ ",
+  " / ____|                                      |  \\/  (_) |     | |        | | |",
+  "| |     __ _ _ __ ___   ___ _ __ ___  _ __   | \\  / |_| |_ ___| |__   ___| | |",
+  "| |    / _` | '_ ` _ \\ / _ \\ '__/ _ \\| '_ \\  | |\\/| | | __/ __| '_ \\ / _ \\ | |",
+  "| |___| (_| | | | | | |  __/ | | (_) | | | | | |  | | | || (__| | | |  __/ | |",
+  " \\_____\\__,__|_| |_| |_|\\___|_|  \\___/|_| |_| |_|  |_|_|\\__\\___|_| |_|\\___|_|_|",                                                                           
+  "<br>",
+  '<span class="color2">Welcome to my interactive web terminal.</span>',
+  "<span class=\"color2\">For a list of available commands, type</span> <span class=\"command\">'help'</span><span class=\"color2\">.</span>",
+];
